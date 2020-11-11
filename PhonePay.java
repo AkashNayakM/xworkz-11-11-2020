9b41661d4d2e1a49392bade53d9d4cf0139f5c4f
AkashNayakM <73881431+AkashNayakM@users.noreply.github.com>
2020-11-11 19:55:22 +0530

    Add files via upload

---
+++ PhonePay.java
@@ -0,0 +1,39 @@
+class PhonePay{
+
+public static void Payment(long Mobileno){
+		
+System.out.println("your mobile recharge done Successful");
+
+}
+	
+public static void Payment(String DTH){
+
+System.out.println("your "+ DTH +" number bill paid successfully");
+
+}
+	
+public static void Payment(int meterid,String name,int amount){
+
+System.out.println("paid electricity bill of"+ meterid +"rupees" + amount +"of" +name);
+
+} 
+	
+public static void Payment(int creditcardno,int amount){
+		
+System.out.println("credit card bill paid successfully for"+ creditcardno +"for amount"+amount);
+
+}
+
+public static void main(String[] args){
+
+PhonePay.Payment(953568425);
+PhonePay.Payment("E-DigitalDTH125692");
+	 
+PhonePay.Payment(172462,"Allu Arjun",1500);
+
+PhonePay.Payment(5634423,3400);
+	
+}
+
+       
+}
